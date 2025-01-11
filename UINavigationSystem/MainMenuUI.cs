af7c3342d727b922ee8a76c1ea3f3e3c4f7930d3
Zerosu <41394193+Zerosuu@users.noreply.github.com>
2025-01-11 15:01:38 +0100

    MainMenuUI comments update

--- UINavigationSystem/MainMenuUI.cs
+++ UINavigationSystem/MainMenuUI.cs
@@ -1,8 +1,10 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+// Subclass of BaseMenuUI used for UI Navigation and various functionality for the main menu
 public class MainMenuUI : BaseMenuUI
 {
+    // Update function listening to button inputs to determine if program should go up a submenu
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
@@ -11,6 +13,7 @@ public class MainMenuUI : BaseMenuUI
         }
     }
 
+    // Function staring the game anew
     public void StartGame()
     {
         SceneManager.LoadScene(1);
